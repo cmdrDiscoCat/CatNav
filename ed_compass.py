ffbeb7e6ed7183be5f1fcbf163a3100587a91286
Discochat <conspirationdesmignons@gmail.com>
2018-02-09 11:27:49 +0100

    Enhancing the UI

--- ed_compass.py
+++ ed_compass.py
@@ -10,62 +10,65 @@ import math
 # init
 
 win = tk.Tk()
-win.title("D'où que je vais")
-win.iconbitmap('ed.ico')
+win.title("ED-Compass")
+#win.iconbitmap('ed.ico')
 win.resizable("False", "False")
+w = '140'
+h = '200'
+win.geometry('{}x{}'.format(w, h))
 
 donneeDestinationLatitude = tk.DoubleVar()
 donneeDestinationLongitude = tk.DoubleVar()
 
-coordonnesActuelles = ttk.LabelFrame(win, text="Votre position actuelle")
+coordonnesActuelles = ttk.LabelFrame(win, text="Votre position", width="140")
 coordonnesActuelles.grid(column=0, row=0)
 
-labelCoordonneesLatitude = ttk.Label(coordonnesActuelles, text="Latitude").grid(column=0, row= 0, sticky=tk.W)
-labelCoordonneesLongitude = ttk.Label(coordonnesActuelles, text="Longitude").grid(column=1, row= 0, sticky=tk.W)
-labelCoordonneesAltitude = ttk.Label(coordonnesActuelles, text="Altitude").grid(column=2, row= 0, sticky=tk.W)
+labelCoordonneesLatitude = ttk.Label(coordonnesActuelles, text="Latitude")
+labelCoordonneesLatitude.grid(column=0, row=0, sticky=tk.W)
+labelCoordonneesLongitude = ttk.Label(coordonnesActuelles, text="Longitude")
+labelCoordonneesLongitude.grid(column=1, row=0, sticky=tk.W)
 
 valueCoordonneesLatitude = ttk.Label(coordonnesActuelles, text="-")
 valueCoordonneesLatitude.grid(column=0, row=1, sticky=tk.W)
 valueCoordonneesLongitude = ttk.Label(coordonnesActuelles, text="-")
 valueCoordonneesLongitude.grid(column=1, row=1, sticky=tk.W)
-valueCoordonneesAltitude = ttk.Label(coordonnesActuelles, text="-")
-valueCoordonneesAltitude.grid(column=2, row=1, sticky=tk.W)
 
-leCap = ttk.LabelFrame(win, text="Cap à suivre")
-leCap.grid(column=0, row=1)
-labelCoordonneesCap = ttk.Label(leCap, text="Actuel")
-labelCoordonneesCap.grid(column=0, row=0, sticky=tk.W)
-valueCoordonneesCap = ttk.Label(leCap, text="-")
-valueCoordonneesCap.grid(column=0, row=1, sticky=tk.W)
-labelLeCap = ttk.Label(leCap, text="Vers destination")
-labelLeCap.grid(column=1, row=0, sticky=tk.W)
-valueLeCap = ttk.Label(leCap, text="-")
-valueLeCap.grid(column=2, row=1, sticky=tk.W)
+destination = ttk.LabelFrame(win, text="Votre destination", width="140")
+destination.grid(column=0, row=1)
 
-destination = ttk.LabelFrame(win, text="Votre destination souhaitée")
-destination.grid(column=0, row=2)
-
-labelDestinationLatitude = ttk.Label(destination, text="Latitude").grid(column=0, row= 0, sticky=tk.W)
-labelDestinationLongitude = ttk.Label(destination, text="Longitude").grid(column=1, row= 0, sticky=tk.W)
+labelDestinationLatitude = ttk.Label(destination, text="Latitude")
+labelDestinationLatitude.grid(column=0, row= 0, sticky=tk.W)
+labelDestinationLongitude = ttk.Label(destination, text="Longitude")
+labelDestinationLongitude.grid(column=1, row= 0, sticky=tk.W)
 
 valueDestinationLatitude = ttk.Entry(destination, width=6,textvariable=donneeDestinationLatitude)
 valueDestinationLatitude.grid(column=0, row=1, sticky=tk.W)
-valueDestinationLongitude = ttk.Entry(destination, width=6,textvariable=donneeDestinationLongitude).grid(column=1, row=1, sticky=tk.W)
+valueDestinationLongitude = ttk.Entry(destination, width=6,textvariable=donneeDestinationLongitude)
+valueDestinationLongitude.grid(column=1, row=1, sticky=tk.W)
 
 valueDestinationLatitude.focus()
+
+leCap = ttk.LabelFrame(win, text="Cap à suivre", relief="sunken", width="140")
+leCap.grid(column=0, row=2)
+valueLeCap = ttk.Label(leCap, text="-")
+valueLeCap.grid(column=0, row=0)
+valueLeCap.configure(font=("Helvetica", "50"))
 
 def refreshPosition(win):
     try:
         with open(r'C:\Users\paind\Saved Games\Frontier Developments\Elite Dangerous\Status.json','r') as infile:
             try:
                 status = json.load(infile)
-                valueCoordonneesLatitude.config(text=status["Latitude"])
-                valueCoordonneesLongitude.config(text=status["Longitude"])
-                valueCoordonneesAltitude.config(text=status["Altitude"])
-                valueCoordonneesCap.config(text=status["Heading"])
+                valueCoordonneesLatitude.config(text=round(status["Latitude"],2))
+                valueCoordonneesLongitude.config(text=round(status["Longitude"],2))
 
                 latStart = status["Latitude"] * math.pi/180;
                 lonStart = status["Longitude"] * math.pi/180;
+
+                if not latStart:
+                    latStart = 0
+                if not lonStart:
+                    lonStart = 0
                 latDest = donneeDestinationLatitude.get() * math.pi/180;
                 lonDest = donneeDestinationLongitude.get() * math.pi/180;
 
